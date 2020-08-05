afddab895f0c8e0683870ae77a908e5f29cf1f0c
Kevin Stewart <kistewart419@gmail.com>
2020-08-04 22:09:59 -0400

    Added some lightweight generics to the objc property completing the tableview controller

--- Contacts-Swift-Hybrid/Contacts-Hybrid/ContactsTableViewController.swift
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/ContactsTableViewController.swift
@@ -24,14 +24,17 @@ class ContactsTableViewController: UITableViewController {
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // TODO: Implement controller count
-		return 0
+        return contactsController.contacts.count
 	}
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
         
         // TODO: Implement display logic
-		
+        let contact = contactsController.contacts[indexPath.row]
+        cell.textLabel?.text = contact.name
+        cell.detailTextLabel?.text = contact.relationship
+        
 		return cell
 	}
 
